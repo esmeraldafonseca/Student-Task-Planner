d91988083020c2b153848c5e147f24a15681b875
Esmeralda Fonseca <esmeralda.fonseca2003@gmail.com>
2026-06-19 12:39:15 +0100

    feat: cria ponto de entrada da aplicação e inicialização do ciclo principal

--- __main__.py
+++ __main__.py
@@ -1,5 +1,19 @@
-from ui import interface
+from customtkinter import CTk
+from services.gestor_tarefas import GestorTarefas
+from ui.interface import InterfaceGrafica
+
+def main():
+    # Inicializa o motor lógico (Services) que carrega o JSON
+    gestor = GestorTarefas()
+    
+    # Inicializa a App CustomTkinter
+    root = CTk()
+    
+    # Inicializa a Interface Gráfica passando as dependências
+    app = InterfaceGrafica(root, gestor)
+    
+    # Inicia o ciclo principal
+    root.mainloop()
 
 if __name__ == "__main__":
-    app= interface.StudentTaskPlanner()
-    app.executar()
+    main()
